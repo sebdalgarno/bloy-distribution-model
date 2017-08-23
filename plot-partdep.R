7345fbbf145ed02f2d7178bb1776a40cc19227dc
sebdalgarno <seb@poissonconsulting.ca>
2017-08-23 14:01:23 -0700

    add units

--- plot-partdep.R
+++ plot-partdep.R
@@ -26,7 +26,7 @@ get_partdep <- function(model = mod[[1]], pred = predictors) {
 
 partdep <- get_partdep(mod[[1]])
 
-plot_cont <- function(data = partdep, pred = "SegLength", log = F, lab = F) {
+plot_cont <- function(data = partdep, pred = "SegLength", log = F, lab = F, title) {
   
   part <- data[[pred]]
   
@@ -36,23 +36,20 @@ plot_cont <- function(data = partdep, pred = "SegLength", log = F, lab = F) {
   gp <- ggplot(part, aes_string(x = pred, y = "y")) + 
     geom_line() + theme_classic() +
     geom_rug(data = dat, aes(x = dat[, pred], y=0), cex = 0.07, sides = "t") +
-    labs(x = pred, y = "") + ylim(0, 0.8) +
+    labs(x = title, y = "") + ylim(0, 0.8) +
     # geom_hline(yintercept = 0, linetype = "dashed", size = 0.25) + 
     theme(text = element_text(size = 16), axis.title.y = element_text(margin = margin(0,15,0,0)), 
           plot.margin = unit(c(0.2, 0.2, 0.7, 0.2), "cm"), axis.text = element_text(size = 8), 
-          axis.title = element_text(size = 11, face = 'bold'), 
+          axis.title = element_text(size = 13), 
           axis.title.x = element_text(margin = margin(7, 0, 0, 0))) 
   
   if (log == T) {
     gp + annotation_logticks(sides="b", short = unit(.5, "mm"), mid = unit(1, "mm"), 
                              long = unit(2, "mm")) +
       scale_x_continuous(labels = scales::math_format(10^.x))
-  } else {}
-  
-  if(lab == T) {
-    gp <- gp + labs(y = "Predicted\nProbability of Occurrence")
-  } else {}
-  gp
+  } else {
+    gp 
+  }
 }
 
 plot_fact <- function(data = partdep, pred = "ShoreType", lab = F) {
@@ -68,7 +65,7 @@ plot_fact <- function(data = partdep, pred = "ShoreType", lab = F) {
     labs(x = "", y = "") + 
     theme(axis.text.y = element_text(hjust = 1, vjust = 0.5), legend.position = "none", 
           axis.text = element_text(size = 11, margin = margin(20, 0, 0, 0)),
-          axis.title = element_text(size = 14, face = 'bold'),
+          axis.title = element_text(size = 14),
           axis.title.x = element_text(margin = margin(10, 0, 0, 0))) +
     scale_y_continuous(expand = c(0,0), limits = c(0, 0.6)) 
   
@@ -78,12 +75,12 @@ plot_fact <- function(data = partdep, pred = "ShoreType", lab = F) {
   gp
 }
 
-seglength <- plot_cont(pred = "SegLength", log = F)
-fetch <- plot_cont(pred = "Fetch",  log = T, lab = T)
-it50 <- plot_cont(pred = "IT50",  log = F)
-it1000 <- plot_cont(pred = "IT1000",  log = F)
-treedist <- plot_cont(pred = "TreeDist", log = T)
-islandarea <- plot_cont(pred = "IslandArea", log = T)
+seglength <- plot_cont(pred = "SegLength", log = F, title = "SegLength (m)")
+fetch <- plot_cont(pred = "Fetch",  log = T, lab = T, title = "Fetch (m)")
+it50 <- plot_cont(pred = "IT50",  log = F, title = expression(paste("IT50 ", "(", 'm'^2, ")"))) 
+it1000 <- plot_cont(pred = "IT1000",  log = F, title = expression(paste("IT1000 ", "(", 'm'^2, ")")))
+treedist <- plot_cont(pred = "TreeDist", log = T, title = "TreeDist (m)")
+islandarea <- plot_cont(pred = "IslandArea", log = T, title = expression(paste("IslandArea ", "(", 'm'^2, ")")))
 
 cont <- plot_grid(treedist, islandarea, fetch, seglength, it50, it1000,
           ncol = 2, align = "v", labels = "AUTO")
